25dd96e6fb1ad5d2dabb5bd550221e24db68dbcc
hadil alalawi <had33l-@outlook.com>
2025-07-22 15:15:08 +0400

    fixing the triagle

---
+++ PatternMaker.java
@@ -0,0 +1,33 @@
+public class PatternMaker {
+    // Print a number triangle
+    public static void printNumberTriangle(int rows) {
+// Print pattern like:
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// (for rows = 4)
+        for (int i = 1; i <= rows; i++) {
+            System.out.println(i + " ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    // Print multiplication table
+    public static void printMultiplicationTable(int number, int limit) {
+// Print: number x 1 = result, number x 2 = result, etc.
+// Up to the limit
+    }
+
+    public static void main(String[] args) {
+        printNumberTriangle(5);
+        //System.out.println();
+      //  printMultiplicationTable(7, 10);
+    }
+
+}
